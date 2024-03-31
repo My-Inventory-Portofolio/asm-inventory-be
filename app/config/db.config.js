2adae35e0e8ed2ab0b1d49678ac7693ef587f80e
Yuta4u <yudhistira4u@gmail.com>
2024-03-31 13:22:22 +0700

    setup pool

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "asm-asset-hardware",
+  // development
+  // HOST: "localhost",
+  // USER: "root",
+  // PASSWORD: "",
+  // DB: "asm-asset-hardware",
+
+  // production
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_DATABASE,
+  PORT: process.env.DB_PORT,
 }
--- app/models/db.js
+++ app/models/db.js
@@ -2,10 +2,17 @@ const mysql = require("mysql")
 const dbConfig = require("../config/db.config.js")
 
 var connection = mysql.createPool({
+  // development
+  // host: dbConfig.HOST,
+  // user: dbConfig.USER,
+  // password: dbConfig.PASSWORD,
+  // database: dbConfig.DB,
+  // production
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
+  port: dbConfig.PORT,
 })
 
 module.exports = connection
