ff7ed185779d5c9caac970f83310a5745fba2c48
Yuta4u <yudhistira4u@gmail.com>
2024-04-03 22:29:56 +0700

    change:db config

--- app/models/db.js
+++ app/models/db.js
@@ -3,18 +3,17 @@ const dbConfig = require("../config/db.config.js")
 
 var connection = mysql.createPool({
   // development
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-
-  // ======================
-  // production
   // host: dbConfig.HOST,
   // user: dbConfig.USER,
   // password: dbConfig.PASSWORD,
   // database: dbConfig.DB,
-  // port: dbConfig.PORT,
+  // ======================
+  // production
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
+  port: dbConfig.PORT,
 })
 
 module.exports = connection
